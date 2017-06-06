2d8e77bec5d7af1dee1c36c795b697b5a12b3bf1
Ali Schlereth <aschlere@sbcglobal.net>
2017-06-06 09:16:07 -0600

    Begin pantry with stock ability

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,21 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_pantry_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_stock_is_an_empty_hash
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_stock_cheese_adds_cheese_to_stock_hash
+    pantry = Pantry.new
+    
+  end
+
 end
